0d399e366f21b2959e733755f053a0c06e935bb9
consultati <eazurara@gmail.com>
2021-01-20 19:23:42 -0300

    Customize MyLayout
    Customize MyLayout & Menu

--- youtube-todo/src/layouts/MainLayout.vue
+++ youtube-todo/src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Todo Windows
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Consultati</div>
       </q-toolbar>
     </q-header>
 
@@ -30,7 +30,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Menu Principal
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -51,47 +51,11 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
-    title: 'Docs',
+    title: 'Todo',
     caption: 'quasar.dev',
     icon: 'school',
     link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+  }  
 ];
 
 export default {
--- youtube-todo/src/pages/Index.vue
+++ youtube-todo/src/pages/Index.vue
@@ -1,14 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="q-pa-md">
+    <!-- Default logo
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
+    -->
+    <h4>lorem</h4>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  // Default Page Index
+  // name: 'PageIndex'  
+
 }
 </script>
