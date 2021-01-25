56107904861c93f73c336de947887cba02116b65
consultati <eazurara@gmail.com>
2021-01-25 19:45:24 -0300

    Using q-editor
    Using q-editor, v-for and methods

--- youtube-todo/src/pages/Index.vue
+++ youtube-todo/src/pages/Index.vue
@@ -1,19 +1,63 @@
 <template>
-  <q-page class="q-pa-md">
-    <!-- Default logo
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-    -->
-    <h4>lorem</h4>
-  </q-page>
+  <div class="q-pa-md q-gutter-sm">
+    <q-editor
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Salve sua tarefa',
+          icon: 'save',
+          label: 'Salvar',
+          handler: saveWork
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"
+    />    
+    <q-card class="row"
+      flat bordered v-for="(item, index) in tasks" :key="index">
+      <q-card-section class="col" v-html="item.texto" :class="item.estado ? 'tachar' : ''" />
+      <q-btn flat color="blue" @click="item.estado = !item.estado">Ação</q-btn>
+      <q-btn flat color="red">Eliminar</q-btn>
+    </q-card>
+  </div>
 </template>
 
 <script>
 export default {
   // Default Page Index
   // name: 'PageIndex'  
+  data() {
+    return {
+      editor: '',
+      tasks: [
+        {texto: 'Tarefa #1', estado: false},
+        {texto: 'Tarefa #2', estado: true},
+        {texto: 'Tarefa #3', estado: false}
+      ]
+    }
+  },
+  methods: {
+    saveWork () {
+      this.tasks.push({
+        texto: this.editor,
+        estado: false
+      })
+      this.$q.notify({
+        message: 'Sua tarefa está salva',
+        color: 'green-4',
+        textColor: 'white',
+        icon: 'cloud_done'
+      })
+    },
+  }
 
 }
 </script>
+
+<style>
+  .tachar{
+    text-decoration: line-through;
+  }
+</style>
